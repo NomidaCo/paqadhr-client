92e12ab14a66f9b28e676a631b053af954033db4
Ifeanyi Ani <ifeanyiani155@gmail.com>
2025-06-29 14:50:44 +0100

    refactor(layout): make the root layout general
    - wrap it with auth-provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
-import "../globals.css";
+import "./globals.css";
+import { AuthProvider } from "@/hooks/use-auth";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,7 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="antialiased font-sans">{children}</body>
+      <body className="antialiased font-sans">
+        <AuthProvider>{children}</AuthProvider>
+      </body>
     </html>
   );
 }
